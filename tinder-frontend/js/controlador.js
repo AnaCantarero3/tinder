34933f6012aa2f2aadb7b4a727fcb105cba55a55
Luis Alejandro Morales <alejandrom646@gmail.com>
2023-03-18 19:08:35 -0600

    Add files via upload

--- tinder-frontend/js/controlador.js
+++ tinder-frontend/js/controlador.js
@@ -1,60 +1,92 @@
 var usuarios = [];
 
+// Obtiene del backend la lista de usuarios
 const obtenerUsuarios = () => {
     fetch('http://localhost:3000/users', {
-      method: 'GET',
-      headers: {
-        "Content-Type": "application/json", //MIME Type
-      }
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json", //MIME Type
+        }
     })
-    .then((respuesta) => respuesta.json())
-    .then((respuestaUsuarios) => {
-      console.log(respuestaUsuarios);
-      usuarios = respuestaUsuarios;
-      renderizarUsuarios();
-    }); 
-  }
+        .then((respuesta) => respuesta.json())
+        .then((respuestaUsuarios) => {
+            console.log(respuestaUsuarios);
+            usuarios = respuestaUsuarios;
+            renderizarUsuarios();
+        });
+}
 
-obtenerUsuarios (); 
+obtenerUsuarios();
+
 const renderizarUsuarios = () => {
-    document.getElementById('select-usuario').innerHTML = '';
+
+    document.getElementById('lista-usuarios').innerHTML = '';
+
     usuarios.forEach(usuario => {
-      document.getElementById('select-usuario').innerHTML +=
-        `<div class="usuario">
-          <img class="imagen-usuario" src="img/${usuario.imagenPerfil}" onclick="mostrarPerfil()">
-          <div>
-            ${usuario.nombre}
-          </div>
-        </div>`;
+        document.getElementById('lista-usuarios').innerHTML +=
+            `<div class="usuario">
+                <img class="imagen-usuario" src="img/${usuario.imagenPerfil}" onclick="mostrarPerfil(${usuario.id})">
+                <div>
+                    ${usuario.nombre}
+                </div>
+            </div>`;
     });
 }
 
-const mostrarPerfil = () => {
-    document.getElementById('lista-usuarios').style.display = "none";
-    document.getElementById('seccion-perfil').style.display = "none";
-    document.getElementById('seccion-matches').style.display = "block";
-    document.getElementById('fuego').style.color = "#FB5064";
-    document.getElementById('obtenerusuarios').style.color = "#DADFE1";
-    document.getElementById('estrella').style.color = "#DADFE1";
-    document.getElementById('seccion-matches').style.display = "none";
-    document.getElementById('logo').style.display = "none";   
-  }
+const mostrarPerfil = (id) => {
+    console.log(id);
 
-const mostrarMatches = () => {
     document.getElementById('lista-usuarios').style.display = "none";
-    document.getElementById('seccion-perfil').style.display = "none";
-    document.getElementById('seccion-matches').style.display = "block";
-    document.getElementById('estrella').style.color = "#FFC400";
-    document.getElementById('obtenerusuarios').style.color = "#DADFE1";
-    document.getElementById('fuego').style.color = "#DADFE1";
-    document.getElementById('logo').style.display = "none";   
-    document.getElementById('seccion-perfil').style.display = "none"; 
-  }
+    document.getElementById('opcion-usuario').classList.remove('active');
+    document.getElementById('contenido-1').style.display = "none";
+    
+    // Estableciendo funcion para botones de matches y perfiles
+    document.getElementById('opcion-perfiles').setAttribute('onclick', 'mostrarPerfiles()')
+    document.getElementById('opcion-matches').setAttribute('onclick', 'mostrarMatches()')
+
+    // Pintando flama de color rojo
+    document.getElementById('opcion-perfiles').classList.add('flama-activa');
+
+    // document.getElementById('opcion-perfiles').style.color = "#FB5064";
+    // document.getElementById('opcion-activa').style.transform = 'translateX(0)';
+    // document.getElementById('seccion-perfil').style.display = "none";
+    // document.getElementById('seccion-matches').style.display = "block";
+    // document.getElementById('obtenerusuarios').style.color = "#DADFE1";
+    // document.getElementById('estrella').style.color = "#DADFE1";
+    // document.getElementById('seccion-matches').style.display = "none";
+    // document.getElementById('logo').style.display = "none";
+}
+
+const mostrarUsuarios = () => {
+
+    document.getElementById('opcion-usuario').classList.add('active');
+    document.getElementById('contenido-1').style.display = "block";
+    document.getElementById('lista-usuarios').style.display = "grid";
+    renderizarUsuarios();
+
+}
+
 
 const mostrarPerfiles = () => {
-    document.getElementById('lista-usuarios').style.display = "block";
-    document.getElementById('seccion-perfil').style.display = "none";
-    document.getElementById('seccion-matches').style.display = "none";
-    document.getElementById('obtenerusuarios').style.color = "#4ECDFF";
-    document.getElementById('logo').style.display = "none";   
+    document.getElementById('opcion-usuario').classList.remove('active');
+    document.getElementById('opcion-perfiles').classList.add('flama-activa');
+    document.getElementById('opcion-matches').classList.remove('estrella-activa');
+    document.getElementById('opcion-activa').style.transform = 'translateX(0)';
+
+    // document.getElementById('contenido-1').style.display = "none";
+    // document.getElementById('contenido-2').style.display = "block";
+    // document.getElementById('contenido-3').style.display = "none";
 }
+
+const mostrarMatches = () => {
+    document.getElementById('opcion-usuario').classList.remove('active');
+    document.getElementById('opcion-perfiles').classList.remove('flama-activa');
+    document.getElementById('opcion-matches').classList.add('estrella-activa');
+    document.getElementById('opcion-activa').style.transform = 'translateX(90%)';
+    
+    // document.getElementById('contenido-1').style.display = "none";
+    // document.getElementById('contenido-2').style.display = "none";
+    // document.getElementById('contenido-3').style.display = "block";
+}
+
+
